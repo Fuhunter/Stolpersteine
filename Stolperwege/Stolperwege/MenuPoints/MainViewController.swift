cf928f3d338cd79e8e5a4fa197519d3a0bcf297d
STiRBx <stirbx@t-online.de>
2016-02-14 20:07:28 +0100

    Parse JSON and Display in Tableview

--- Stolperwege/Stolperwege/MenuPoints/MainViewController.swift
+++ Stolperwege/Stolperwege/MenuPoints/MainViewController.swift
@@ -11,9 +11,12 @@ import Alamofire
 import SwiftyJSON
 
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
+	@IBOutlet weak var tabelView: UITableView!
+	
+	var persons: JSON = nil
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -34,6 +37,8 @@ class MainViewController: UIViewController {
         }
         
         Alamofire.request(.GET, testLink, parameters: nil, encoding: .JSON, headers: nil).response { (request, response, data, error) -> Void in
+			UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+			
             guard let rdata = data else {
                 return
             }
@@ -45,8 +50,35 @@ class MainViewController: UIViewController {
             }
 			
             
-            let persons = JSON.parse(jsonString as String)
+            self.persons = JSON.parse(jsonString as String)
+			self.tabelView.reloadData()
+			
+			UIApplication.sharedApplication().networkActivityIndicatorVisible = false
         }
     }
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return self.persons.count
+	}
+	
+	func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+		return true
+	}
+	
+	func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+		// FIXME
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("personCell")! as UITableViewCell
+		
+		let item = self.persons[indexPath.row]
+		
+		cell.textLabel!.text = item["name"].string!
+		
+		cell.accessoryType = .DisclosureIndicator
+		
+		return cell
+	}
     
 }
